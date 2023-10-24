7ecf9381f0abb6b0c2a41dc997185bdb821db248
edmalfizeo <edmalfizeo@gmail.com>
2023-10-23 22:28:23 -0300

    Adicionando Recursos nativos e Exercicio1 JS

---
+++ Fundamentos do JS/JavaScript 1/Exercicio 1 Javascript/index.js
@@ -0,0 +1,27 @@
+//Exercicio 1 , tirar os comentarios para rodar.
+/*let nome = prompt("Digite seu Nome: ")
+let sobrenome = prompt("Digite seu Sobrenome: ")
+let campo = prompt("Digite seu campo de estudo: ")
+let ano = prompt("Digite o ano do seu nascimento: ") 
+
+alert("Nome: " + nome + "\n" + "Sobrenome : " + sobrenome + "\n" + "Campo de estudo: " + campo + "\n" + "Idade: " + (2023 - ano))
+*/
+
+// Exercicio 2, tirar os comentarios para rodar.
+
+/*let num1 = prompt("Digite o Primeiro numero: ")
+let num2 = prompt("Digite o Segundo numero: ")
+
+let x = parseFloat(num1)
+let y = parseFloat(num2)
+
+const soma = (x + y)
+const sub = (x - y)
+const mult =  (x * y)
+const div =  (x / y)
+
+alert("Soma: " + soma + "\n" +
+     "Subtração: " + sub + "\n" +
+     "Multiplicação: " + mult + "\n" +
+     "Divisão: " + div)
+*/
---
+++ Fundamentos do JS/JavaScript 1/Recursos Nativos do Navegador/index.js
@@ -0,0 +1,13 @@
+let nome = "Eduardo"
+
+console.log(nome) 
+console.error(nome)
+console.warn(nome)
+
+
+//window.alert(nome)
+//nome = window.prompt("Insira o seu nome: ")
+//let confirmacao = window.confirm("Confirma essa ação?")
+
+
+
