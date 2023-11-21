923a45e44124ab8f824799099ac530680d68bdc7
edmalfizeo <edmalfizeo@gmail.com>
2023-11-21 15:39:21 -0300

    Adicionando For e Exercicio Final

---
+++ Fundamentos do JS/JavaScript 1/Exercicio Final modulo 1 Javascript/index.js
@@ -0,0 +1,25 @@
+//Tirar o comentario para rodar o Exercicio Final parte 1
+/*
+const numero = parseFloat(prompt("Digite um numero de 1 a 20: "));
+let resultado = "";
+for(let indice = 0; indice <= 20; indice++){
+    resultado += numero + " * " + indice + " = " + (numero * indice) + "\n"
+}
+alert(resultado);
+*/
+
+//Tirar o comentario para rodar o Exercicio Final parte 2
+/*
+const palavra = prompt("Informe uma palavra:")
+let palavraInvertida = ""
+
+for (let i = palavra.length - 1; i >= 0; i--) {
+  palavraInvertida += palavra[i]
+}
+
+if (palavra === palavraInvertida) {
+  alert(palavra + " é um palíndromo!")
+} else {
+  alert(palavra + " não é um palíndromo!\n\n" + palavra + " !== " + palavraInvertida)
+}
+*/
---
+++ Fundamentos do JS/JavaScript 1/For/index.js
@@ -0,0 +1,8 @@
+for(let indice = 1; indice <= 10; indice++) {
+    alert("Indice = " + indice);
+}
+
+let nome = "Eduardo"
+for(let indice = 0; indice < nome.length; indice++) {
+    console.log(nome[indice])
+}
