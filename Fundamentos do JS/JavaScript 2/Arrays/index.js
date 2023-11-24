e1dc6bfd4bab4d95201fa2163b10785c4adcd85f
edmalfizeo <edmalfizeo@gmail.com>
2023-11-24 16:29:56 -0300

    Adicionando JavaScript2 Array

---
+++ Fundamentos do JS/JavaScript 2/Arrays/index.js
@@ -0,0 +1,12 @@
+const listaDeCompras = ["Açucar", false]
+console.log(listaDeCompras)
+
+listaDeCompras[0] = "Arroz"
+listaDeCompras[1] = "Feijão"
+listaDeCompras[3] = 12
+listaDeCompras[5] = "Batata"
+
+console.log(listaDeCompras)
+
+console.log(listaDeCompras[4])
+
